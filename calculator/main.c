0d80e19e1bf0dc6fd5bc6bfd5e913abfe1793ec7
Nicolas Sicard <nicolas@sicard.me>
2021-08-07 17:01:10 +0200

    feat: cli exec

--- calculator/main.c
+++ calculator/main.c
@@ -1,3 +1,38 @@
-int main(args) {
+#include "calculation.h"
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    float (*operations[4])(float, float);
+    operations[0] = sum;
+    operations[1] = substract;
+    operations[2] = multiply;
+    operations[3] = divide;
+
+    float a, b, result;
+    int choice;
+
+    char *keepGoing = "";
+    do {
+        printf("Type 0 to add, 1 to substract, 2 to multiply, 3 to divide, 4 to quit: ");
+        scanf("%d", &choice);
+
+        if (choice == 4) {
+            break;
+        }
+
+        printf("First number: ");
+        scanf("%f", &a);
+
+        printf("Second number: ");
+        scanf("%f", &b);
+
+        result = operations[choice](a, b);
+        printf("The result is: %f\n", result);
+    } while (true);
+
+    printf("C ya!\n");
+    return 0;
 }
